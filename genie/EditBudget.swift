0a9060add892c9dda31cd1fd2eb0a9f6bb03a58e
Michelle Ly <lymichelle2016@icloud.com>
2022-01-06 18:07:09 -0500

    Updated Edit Budget Screen

--- genie/EditBudget.swift
+++ genie/EditBudget.swift
@@ -8,20 +8,42 @@
 import SwiftUI
 
 struct EditBudget: View {
-    @State var newBudget = "";
+    @State var newBudget = "$278.00";
     
     var body: some View {
-        Form {
-            TextField("New Budget Amount", text: $newBudget)
-                .keyboardType(.numberPad)
-            Button("Save", action: {/* add item to firebase */})
-        }
-        .navigationTitle("Add Item")
+        NavigationView{
+            VStack{
+                Spacer()
+                Form {
+                    Text("Current Budget: ")
+                    TextField("New Budget Amount", text: $newBudget)
+                        .keyboardType(.numberPad)
+                    Button("Save", action: {/* add item to firebase */})
+                }
+                Spacer()
+                Text("Genie Cat's Tips for Setting a Budget")
+                    .padding()
+                    .font(.system(size: 18, weight: .semibold))
+                Text("- Consider the 50/20/30 budget rule: Divide each paycheck into 50% for the essentials, 20% for savings and 30% for everything else")
+                    .padding()
+                    .font(.system(size: 16, weight: .light))
+                Text("- Remember that the budget for each month can vary and adjust accordingly. Expect the unexpected and decide how much to save.")
+                    .padding()
+                    .font(.system(size: 16, weight: .light))
+                Link("More Information on Money Management",
+                      destination: URL(string: "https://www.capitalone.com/bank/money-management/")!)
+                Image("Genie.png")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+            }
+        .navigationTitle("Edit Budget Item")
     }
 }
 
 struct EditBudget_Previews: PreviewProvider {
     static var previews: some View {
         EditBudget()
+        }
     }
 }
