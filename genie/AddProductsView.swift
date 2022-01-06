9b51b713a0dc7eb940f463c6b9b879edac951f18
Nirranjan Akilan <anirranjan@gmail.com>
2022-01-06 13:05:47 -0600

    Added Image Picker

--- genie/AddProductsView.swift
+++ genie/AddProductsView.swift
@@ -12,6 +12,8 @@ struct AddProductsView: View {
     @State var productPrice = "";
     @State var purchaseLink: String = "";
     @State var preference = "";
+    @State private var image = UIImage();
+    @State private var showSheet = false;
     
     var body: some View{
         Form {
@@ -21,6 +23,19 @@ struct AddProductsView: View {
             TextField("Purchase Link", text: $purchaseLink)
             TextField("Preference", text: $preference)
                 .keyboardType(.numberPad)
+            HStack {
+                Image(uiImage:self.image)
+                    .resizable()
+                    .cornerRadius(50)
+                    .frame(width:100,height:100)
+                    .background(Color.black.opacity(0.2))
+                    .aspectRatio(contentMode:.fill)
+                    .clipShape(Circle())
+                Button("Select Photo", action: {showSheet = true})
+            }.padding(.horizontal, 20)
+                .sheet(isPresented: $showSheet){
+                    ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+                }
             Button("Save", action: {/* add item to firebase */})
         }
         .navigationTitle("Add Item")
