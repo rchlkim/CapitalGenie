9f739aaab7916a1e9250f3969d1ccd5eaa6c90e9
Nirranjan Akilan <anirranjan@gmail.com>
2022-01-05 23:27:04 -0600

    Initial Commit

---
+++ genie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  genie
+//
+//  Created by Nirranjan Akilan on 1/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ genie/genieApp.swift
@@ -0,0 +1,17 @@
+//
+//  genieApp.swift
+//  genie
+//
+//  Created by Nirranjan Akilan on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct genieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
